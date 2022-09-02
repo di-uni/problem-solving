b7010ec180c9d274a5078017a352fe88b25700c6
di-uni <jiyunp57@gmail.com>
2022-09-02 17:02:41 +0900

    Solve kakao test problems

---
+++ programmers/kakao/2021 blind/메뉴 리뉴얼.py
@@ -0,0 +1,45 @@
+from itertools import combinations 
+from collections import defaultdict
+
+def solution(orders, course):
+    answer = []
+    course_dict = defaultdict(int)
+    
+    for num in course:
+        for order in orders:
+            if len(order) < num:
+                continue
+            alpha_order = ''.join(sorted(order))
+            for menu in list(combinations(alpha_order, num)):
+                course_dict[''.join(menu)] += 1
+        # print(course_dict)
+        if course_dict:
+            max_value = max(course_dict.values())
+            if max_value > 1:
+                answer += [key for key, value in course_dict.items() if value == max_value]
+                # print(answer)
+        course_dict = defaultdict(int)
+         
+        
+    return sorted(answer)
+
+
+# ======================================
+# Other's solution 
+
+
+import collections
+import itertools
+
+def solution(orders, course):
+    result = []
+
+    for course_size in course:
+        order_combinations = []
+        for order in orders:
+            order_combinations += itertools.combinations(sorted(order), course_size)
+
+        most_ordered = collections.Counter(order_combinations).most_common()
+        result += [ k for k, v in most_ordered if v > 1 and v == most_ordered[0][1] ]
+
+    return [ ''.join(v) for v in sorted(result) ]
---
+++ programmers/kakao/2022 internship/성격 유형 검사하기.py
@@ -0,0 +1,58 @@
+def solution(survey, choices):
+    types = ['RT', 'CF', 'JM', 'AN']
+    result = {}
+    answer = ''
+    
+    for choice, score in zip(survey, choices):
+        score -= 4
+        if score < 0:
+            if choice[0] not in result:
+                result[choice[0]] = 0
+            result[choice[0]] -= score
+        if score > 0:
+            if choice[1] not in result:
+                result[choice[1]] = 0
+            result[choice[1]] += score
+    
+    # print(result)
+    
+    for ty in types:
+        res_ty = [0, 0]
+        if ty[0] in result:
+            res_ty[0] = result[ty[0]]
+        if ty[1] in result:
+            res_ty[1] = result[ty[1]]
+        if res_ty[0] < res_ty[1]:
+            answer += ty[1]
+        else: 
+            answer += ty[0]
+    return answer
+
+
+
+
+
+# ======================================
+# Other's solution 
+
+
+def solution(survey, choices):
+
+    my_dict = {"RT":0,"CF":0,"JM":0,"AN":0}
+    for A,B in zip(survey,choices):
+        if A not in my_dict.keys():
+            A = A[::-1]
+            my_dict[A] -= B-4
+        else:
+            my_dict[A] += B-4
+
+    result = ""
+    for name in my_dict.keys():
+        if my_dict[name] > 0:
+            result += name[1]
+        elif my_dict[name] < 0:
+            result += name[0]
+        else:
+            result += sorted(name)[0]
+
+    return result
