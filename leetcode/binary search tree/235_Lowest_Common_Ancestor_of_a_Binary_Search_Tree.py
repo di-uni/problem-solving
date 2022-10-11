9d33367c45cffb5140d7aad5392faec1108762ee
Jiyun Park <jiyunp57@gmail.com>
2022-10-12 03:27:52 +0900

    LeetCode 75 Day 8

---
+++ leetcode/binary search tree/235_Lowest_Common_Ancestor_of_a_Binary_Search_Tree.py
@@ -0,0 +1,28 @@
+# 2022.10.11
+# First Trial
+# Test Passed
+# Runtime: faster than 24.74%, Memory Usage: less than 14.03%
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        node = root
+        
+        while node:
+            # print(node.val, node)
+            if node.val > p.val and node.val > q.val:
+                node = node.left
+                continue
+            elif node.val < p.val and node.val < q.val:
+                node = node.right
+                continue
+            else:
+                break
+        
+        return node
---
+++ leetcode/binary search tree/98_Validate_Binary_Search_Tree.py
@@ -0,0 +1,43 @@
+# 2022.10.11
+# First Trial
+# Test Passed
+# Runtime: faster than 24.74%, Memory Usage: less than 14.03%
+
+from typing import Optional, List
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+        
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        def inorder(array: List[int], root: Optional[TreeNode]):
+            if root.left:
+                inorder(array, root.left)
+            array.append(root.val)
+            # print(root.val)
+            if root.right:
+                inorder(array, root.right)
+            
+        inorderlist = []
+        inorder(inorderlist, root)
+        # print(inorderlist)
+        
+        return inorderlist == sorted(list(set(inorderlist)))
+
+
+
+# =================================================================
+# Other's Solution
+# Using recursive
+# Runtime: faster than 94.23%, Memory Usage: less than 80.26%
+
+    def isValidBST(self, root: Optional[TreeNode], floor=float("-inf"), ceiling=float("inf")) -> bool:
+        if not root:
+            return True
+        if not floor < root.val < ceiling:
+            return False
+        return self.isValidBST(root.left, floor, root.val) and self.isValidBST(root.right, root.val, ceiling)
